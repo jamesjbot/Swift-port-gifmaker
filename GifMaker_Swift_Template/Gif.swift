c1c3ea7f03cbda087281576d647230e9c67bbc22
jamesjbot <jongs.j@gmail.com>
2017-03-17 17:12:19 -0400

    feature: Make Gif class conform to NSObject, NSCoding protocols in preparation for future storage
    We need to be able to save Gifs. So we need to implement encoder and decoder methods.

--- GifMaker_Swift_Template/Gif.swift
+++ GifMaker_Swift_Template/Gif.swift
@@ -9,20 +9,37 @@
 import Foundation
 import UIKit
 
-class Gif{
+class Gif: NSObject, NSCoding {
     
     let url: URL
     let videoURL: URL
     var caption: String?
-    let gifImage: UIImage?
+    let gifImage: UIImage
     var gifData: NSData?
     
     init(url: URL, videoURL: URL, caption: String?){
         self.url = url
         self.videoURL = videoURL
         self.caption = caption
-        self.gifImage = UIImage.gif(url: url.absoluteString)
+        self.gifImage = UIImage.gif(url: url.absoluteString)!
         self.gifData = nil
     }
+
+    required init?(coder aDecoder: NSCoder) {
+        url = aDecoder.decodeObject(forKey: "url") as! URL
+        videoURL = aDecoder.decodeObject(forKey: "videoURL") as! URL
+        caption = aDecoder.decodeObject(forKey: "caption") as? String
+        gifImage = aDecoder.decodeObject(forKey: "gifimage") as! UIImage
+        gifData = aDecoder.decodeObject(forKey: "gifdata") as? NSData
+    }
+
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(self.url, forKey: "url")
+        aCoder.encode(videoURL, forKey: "videoURL")
+        aCoder.encode(caption, forKey: "caption")
+        aCoder.encode(gifImage, forKey: "gifimage")
+        aCoder.encode(gifData, forKey: "gifdata")
+    }
+
 
 }
