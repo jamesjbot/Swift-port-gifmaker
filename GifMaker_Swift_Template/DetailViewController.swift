bab249d63293bdee972fedfc17fb9e03fb7d0afa
jamesjbot <jongs.j@gmail.com>
2017-03-17 19:03:44 -0400

    feature: Add DetailViewController to see previously created gifs
    We need to see our old gifs. This view controller fulfills that need.

---
+++ GifMaker_Swift_Template/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  GifMaker_Swift_Template
+//
+//  Created by James Jongsurasithiwat on 3/17/17.
+//  Copyright © 2017 Gabrielle Miller-Messner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // MARK: - Variables
+
+    var localGif: Gif?
+
+    // MARK: - IBActions
+
+    @IBAction func shareGif(_ sender: UIButton) {
+        share(thisGif: localGif!)
+    }
+
+    @IBAction func dismissViewController(_ sender: UIButton) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+
+    // MARK: - IBOutlets
+
+    @IBOutlet weak var gifImageView: UIImageView!
+
+    // MARK: - Functions
+
+
+    // MARK: - View Life Cycle Functions
+
+    override func viewDidLoad() {
+        gifImageView.image = localGif?.gifImage
+    }
+
+}
